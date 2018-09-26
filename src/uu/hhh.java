8e0e7721ca90f4710ad9786668d07c31e16fe72c
fudanbao <964906161@qq.com>
2018-09-26 20:37:07 +0800

    初次提交，请多多指教，github

--- src/uu/hhh.java
+++ src/uu/hhh.java
@@ -3,7 +3,7 @@ package uu;
 public class hhh {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println();
+        System.out.println(2222);
 
     }
 }
